2aad1e98710d1329e5a99f39281555863cab9282
Aliexpress228 <59725483+Aliexpress228@users.noreply.github.com>
2021-03-05 22:17:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-
-sleep(500).then(() => {
-});
 $('document').ready(function(){
     jQuery('.teamate').on('click',function(){
        $('#middle').append('<h4 class="opps" style="color:red;">Извините. Но клуб МОЛОДЫХ И ПЕРСПЕКТИВНЫХ открыт только для МОЛОДЫХ И ПЕРСПЕКТИВНЫХ</h4>');
-        sleep(3000).then(() => {
+        sleep(5000).then(() => {
             $( ".opps" ).remove();
     });
     });
